4279e4ef54aad2b7a1b55b450c5eec985baff0ff
Kyoko <108702599+KyoMatias@users.noreply.github.com>
2023-11-17 00:03:21 +0800

    updated

---
+++ Matias/finals.js
@@ -0,0 +1,24 @@
+function toggleE1() {
+    var E1div = document.getElementById('ef1');
+    if (E1div.style.display === 'none' || E1div.style.display === '') {
+        E1div.style.display = 'block';
+    } else {
+        E1div.style.display = 'none';
+    }
+}
+function toggleE2() {
+    var E2div = document.getElementById('ef2');
+    if (E2div.style.display === 'none' || E2div.style.display === '') {
+        E2div.style.display = 'block';
+    } else {
+        E2div.style.display = 'none';
+    }
+}
+function toggleE3() {
+    var E3div = document.getElementById('ef1');
+    if (E3div.style.display === 'none' || E3div.style.display === '') {
+        E3div.style.display = 'block';
+    } else {
+        E3div.style.display = 'none';
+    }
+}
